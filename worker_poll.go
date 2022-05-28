98fb534a0f6c0b70384b764d7817b6f0908bdfa2
Daniel Andreyev <anboo@users.noreply.github.com>
2022-05-28 12:54:39 +0300

    Update worker_poll.go

--- worker_poll.go
+++ worker_poll.go
@@ -1,51 +1,98 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"sync"
+	"time"
 )
 
 type WorkerPool struct {
 	wg       *sync.WaitGroup
-	taskChan chan func()
+	taskChan chan func(ctx context.Context)
+	ctx      context.Context
+	duration *time.Duration
+
+	mDuration *sync.RWMutex
+}
+
+func (wp *WorkerPool) Timeout(duration time.Duration) {
+	wp.mDuration.Lock()
+	wp.duration = &duration
+	wp.mDuration.Unlock()
 }
 
 // NewWorkerPool create new WorkerPoll
 // and create maxConcurrentlyWorkers goroutines which wait for new tasks
-func NewWorkerPool(maxConcurrentlyWorkers int) *WorkerPool {
-	wg := &WorkerPool{
-		wg:       &sync.WaitGroup{},
-		taskChan: make(chan func()),
+// if you use timeout functional worker pool will need run +2 goroutine for checking it (timeout goroutine and context.WithCancel goroutine)
+func NewWorkerPool(maxConcurrentlyWorkers int, ctx context.Context) *WorkerPool {
+	wp := &WorkerPool{
+		wg:        &sync.WaitGroup{},
+		taskChan:  make(chan func(ctx context.Context)),
+		ctx:       ctx,
+		mDuration: &sync.RWMutex{},
 	}
 
 	for j := 0; j < maxConcurrentlyWorkers; j++ {
-		wg.RunWorker()
+		wp.RunWorker()
 	}
 
-	return wg
+	return wp
 }
 
 // RunWorker run goroutine and fetch all tasks
 func (wp *WorkerPool) RunWorker() {
-	go func(wg *sync.WaitGroup) {
+	go func() {
 		for task := range wp.taskChan {
-			func() {
-				defer wg.Done()
+			job := func(ctx context.Context, t func(ctx context.Context)) {
+				defer wp.wg.Done()
 
 				// if task will throw panic
 				// we do wg.Done() anyway
 				defer func() {
-					if recover() != nil {
+					r := recover()
+
+					if r != nil {
+						fmt.Println("panic from task worker poll ", r)
 					}
 				}()
 
-				task()
-			}()
+				t(ctx)
+			}
+
+			wp.mDuration.RLock()
+			if wp.duration != nil {
+				res := make(chan bool, 1)
+
+				lCtx, cancel := context.WithCancel(wp.ctx)
+
+				go func(task func(ctx context.Context)) {
+					job(lCtx, task)
+					res <- true
+					close(res)
+				}(task)
+
+				select {
+				case <-time.After(*wp.duration):
+					wp.mDuration.RUnlock()
+					fmt.Println("worker poll error timeout")
+					cancel()
+					continue
+				case <-res:
+					wp.mDuration.RUnlock()
+					cancel()
+					close(res) //Fix context leak
+				}
+			} else {
+				wp.mDuration.RUnlock()
+				job(wp.ctx, task)
+			}
 		}
-	}(wp.wg)
+	}()
 }
 
 // AddTask delay execution function
-func (wp *WorkerPool) AddTask(f func()) {
+func (wp *WorkerPool) AddTask(f func(ctx context.Context)) {
 	wp.wg.Add(1)
 	wp.taskChan <- f
 }
